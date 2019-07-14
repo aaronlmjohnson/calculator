6d170592ceb7a7217037abbe8a654d9c5ebad5bd
ajonaaronajon <ajonaaronajon@yahoo.com>
2019-07-14 15:48:18 -0500

    created basic body for calc and basic functions

---
+++ main.js
@@ -0,0 +1,132 @@
+const buttons = document.querySelector('#buttons');
+
+for(let i = 0; i < 18; i++){
+  let button = document.createElement('div');
+  let num = document.createElement('p');
+  //num.textContent = i < 10 ? i : i === 10 ? '=' : i === 11 ? 'clear'
+      // : i === 12 ? '+' : i === 13 ? '-' :i === 14 ? '*' : '/';
+  switch(true){
+    case (i < 10):
+      num.textContent = i;
+      button.classList.add('number');
+      break;
+    case (i === 10):
+      num.textContent = '+';
+      button.setAttribute('id', 'add');
+      break;
+    case (i === 11):
+      num.textContent = '-';
+      button.setAttribute('id', 'subtract');
+      break;
+    case (i === 12):
+      num.textContent = '*';
+      button.setAttribute('id', 'multiply');
+      break;
+    case (i === 13):
+      num.textContent = '/';
+      button.setAttribute('id', 'divide');
+      break;
+    case (i === 14):
+      num.textContent = '=';
+      button.setAttribute('id', 'operate');
+      break;
+    case (i === 15):
+      num.textContent = 'clear';
+      button.setAttribute('id', 'clear');
+      break;
+    case (i === 16):
+      num.textContent = '.';
+      button.setAttribute('id', 'decimal');
+      break;
+    case (i === 17):
+      num.textContent = 'neg';
+      button.setAttribute('id', 'negative');
+  }    
+
+  button.classList.add('button');
+  button.append(num);
+  buttons.append(button);
+  
+}
+const buttonsArr = [...document.querySelectorAll('.button')];
+const myMath = {};
+
+myMath.add = (a, b) => Number(a) + Number(b);
+myMath.subtract = (a, b) => a - b;
+myMath.multiply = (a, b) => a * b;
+myMath.divide = (a, b) => a / b;
+myMath.clear = ()=>{
+  expression = [];
+  expressionStr = '';
+  currentNum = '';
+  currentSign = '';
+  signIndex = '';
+}
+myMath.operate = (operator, a, b) =>  {
+  if(operator === '/')
+    return myMath.divide(a,b);
+  else if(operator === '*')
+    return myMath.multiply(a,b);
+  else if(operator === '+')
+    return myMath.add(a,b);
+  else if(operator === '-')
+    return myMath.subtract(a,b);
+
+};
+let displayToScreen = (content)=>{screenValue.textContent = content;}
+let clearScreen = ()=>{screenValue.textContent = '';}
+
+const equals = document.querySelector('#operate');
+const screenValue = document.querySelector('#screenValue');
+let currentNum = '';
+let currentSign = '';
+let signIndex = '';
+let expression = [];
+let expressionStr = ''; // needed to keep track of the values that are being displayed on the screen before calculation
+let signRegex = /[\+\-\/\*]/gm;
+
+buttonsArr.forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    if(button.textContent === '.' && currentNum.indexOf('.') > -1)
+      return;
+    if(button.textContent === 'clear'){
+      myMath.clear();
+      clearScreen();
+      return;
+    }
+    if(button.textContent === '='){
+      if(currentNum){
+        expression.push(currentNum);
+        currentNum = '';
+      }
+      while(expression.length > 1){
+        if(expression.indexOf('/') !== -1)
+          currentSign = '/';
+        else if(expression.indexOf('*') !== -1)
+          currentSign = '*';
+        else if(expression.indexOf('+') !== -1)
+          currentSign = '+';
+        else if(expression.indexOf('-') !== -1)
+          currentSign = '-';
+
+        signIndex = expression.indexOf(currentSign);
+        expression.splice(signIndex - 1, 3, myMath.operate(currentSign, expression[signIndex - 1], expression[signIndex + 1])); 
+      }
+      displayToScreen(expression[0]);
+      myMath.clear();
+
+    } else if (!!button.textContent.match(signRegex) && !!currentNum){
+        expression.push(currentNum);
+        expression.push(button.textContent);
+        expressionStr += button.textContent;
+        currentNum = '';
+    } else if(!button.textContent.match(signRegex)){
+        currentNum += button.textContent;
+        expressionStr += button.textContent;
+    }
+    console.log(expression);
+    if(button.textContent !== '=')
+      displayToScreen(expressionStr);
+  })
+})
+
